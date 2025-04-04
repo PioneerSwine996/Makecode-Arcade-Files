20921ecc0b4fa7048867a8bb8c090a4be3753069
PioneerSwine996 <129558257+PioneerSwine996@users.noreply.github.com>
2025-04-04 09:20:12 -0700

    stuff

--- main.ts
+++ main.ts
@@ -1,24 +1,12 @@
-/**
- * This is the main file for your project.
- *
- * Create images, tilemaps, animations, and songs using the
- * asset explorer in VS Code. You can reference those assets
- * using the tagged templates on the assets namespace:
- *
- *     assets.image`myImageName`
- *     assets.tilemap`myTilemapName`
- *     assets.tile`myTileName`
- *     assets.animation`myAnimationName`
- *     assets.song`mySongName`
- *
- * New to MakeCode Arcade? Try creating a new project using one
- * of the templates to learn about Sprites, Tilemaps, Animations,
- * and more! Or check out the reference docs here:
- *
- * https://arcade.makecode.com/reference
- */
-
-game.onUpdate(() => {
-    // Code in this function will run once per frame. MakeCode
-    // Arcade games run at 30 FPS
-});
+function changeGreeting(): void {
+    let greeting = `Salut à tous !`
+    let letters: number = 0
+    findLength(greeting)
+}
+function findLength(name: string) {
+    let letters: number = name.length
+}
+let greeting: string = `Hello, World!`
+let DuckSprite: Sprite = sprites.create(sprites.duck.duck6, SpriteKind.Player)
+changeGreeting()
+DuckSprite.say(greeting)
