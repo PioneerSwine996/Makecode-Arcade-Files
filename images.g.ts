18559b77260a6f11c7eef9eaecae3d552c0e2477
PioneerSwine996 <129558257+PioneerSwine996@users.noreply.github.com>
2025-04-09 09:02:33 -0700

    Half Done

---
+++ images.g.ts
@@ -0,0 +1,33 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "mySong":return hex`008c0004080200`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
+
+// Auto-generated code. Do not edit.
+namespace myTiles {
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,24 +1,15 @@
-/**
- * This is the main file for your project.
- *
- * Create images, tilemaps, animations, and songs using the
- * asset explorer in VS Code. You can reference those assets
- * using the tagged templates on the assets namespace:
- *
- *     assets.image`myImageName`
- *     assets.tilemap`myTilemapName`
- *     assets.tile`myTileName`
- *     assets.animation`myAnimationName`
- *     assets.song`mySongName`
- *
- * New to MakeCode Arcade? Try creating a new project using one
- * of the templates to learn about Sprites, Tilemaps, Animations,
- * and more! Or check out the reference docs here:
- *
- * https://arcade.makecode.com/reference
- */
+let melody: string[] = [
+    "E D C D",
+    "E E E:8",
+    "D D D:8",
+    "E G G:8",
+    "E D C D",
+    "E E E E",
+    "D D E D",
+    "C:16"
 
-game.onUpdate(() => {
-    // Code in this function will run once per frame. MakeCode
-    // Arcade games run at 30 FPS
-});
+
+]
+for (let i = 0; i < melody.length; i++){
+    music.playMelody(melody[i], 80)
+}
