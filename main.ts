544595f3426069c855c706ee777d9dac663ce071
PioneerSwine996 <129558257+PioneerSwine996@users.noreply.github.com>
2025-04-11 08:58:38 -0700

    Completed Lab 3.4

--- main.ts
+++ main.ts
@@ -1,24 +1,46 @@
-/**
- * This is the main file for your project.
- *
- * Create images, tilemaps, animations, and songs using the
- * asset explorer in VS Code. You can reference those assets
- * using the tagged templates on the assets namespace:
- *
- *     assets.image`myImageName`
- *     assets.tilemap`myTilemapName`
- *     assets.tile`myTileName`
- *     assets.animation`myAnimationName`
- *     assets.song`mySongName`
- *
- * New to MakeCode Arcade? Try creating a new project using one
- * of the templates to learn about Sprites, Tilemaps, Animations,
- * and more! Or check out the reference docs here:
- *
- * https://arcade.makecode.com/reference
- */
+// #1
+let numbers: number[] = [8, 6, 7, 5, 3, 0, 9, 42]
+let states: string[] = ["Michigan", "California",
+    "Missouri", "Washington",
+    "Illinois", "Georgia"
+]
+console.log("Numbers:")
+console.log(numbers)
+console.log("States:")
+console.log(states)
+// #2
+function numberSearch(array: number[], searchTerm: number): number {
+    for (let i = 0; i < array.length; i++) {
+        // #3
+        if (array[i] == searchTerm) {
+            return array.indexOf(array[i])
+        }
+    }
+    // #4
+    return -1
+}
+console.log(numberSearch(numbers, 3))
+// #5
+// #6
+function arraySearch<T>(array: T[], toFind: T): number {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == toFind) {
+            return array.indexOf(array[i])
+        }
+    }
+    return -1
+}
+console.log(arraySearch(numbers, 5))
 
-game.onUpdate(() => {
-    // Code in this function will run once per frame. MakeCode
-    // Arcade games run at 30 FPS
-});
+states.sort()
+console.log("Sorted strings:")
+console.log(states)
+// #7
+numbers.sort()
+console.log("Sorted numbers?")
+console.log(numbers)
+// #8
+console.log(numbers.sort(function (value1: number, value2: number): number {
+    return value1 - value2
+}))
+
