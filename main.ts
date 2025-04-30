d6fab2a649d04c794e305d34f52d4c1318483473
PioneerSwine996 <129558257+PioneerSwine996@users.noreply.github.com>
2025-04-30 08:49:20 -0700

    Completed Lab 4.1

--- main.ts
+++ main.ts
@@ -1,29 +1,111 @@
+// #15
+// ======================= GLOBAL VARIABLES======================= \\
 let player: Sprite = null;
 let projectile: Sprite = null;
-let enemy: sprites.ExtendableSprite = null;
-
+let food: Sprite = null;
+let ghostenemy: EnemySprite = null;
+let otherenemy: EnemySprite = null;
+let FoodEaten: Boolean = false;
+// ======================= CONSTANTS ======================= \\
+// #14
+const playerXLocation = 120
+const playerYLocation = 80
+const ghostenemyHitpoints = 2
+const ghostenemyXLocation = 80
+const ghostenemyYLocation = 8
+const otherenemyHitpoints = 5
+const otherenemyXLocation = 40
+const otherenemyYLocation = 60
+const projectileVelocity = -100
+// ======================= NAMESPACES ======================= \\
+namespace SpriteKind {
+    export const otherEnemy = SpriteKind.create()
+}
+// ======================= CLASSES ======================= \\
+// #7
+// #8
+class EnemySprite extends sprites.ExtendableSprite {
+    hitpoints: number
+}
+// ======================= VARIABLE DEFINITION ======================= \\
+// #12
+food = sprites.create(assets.image`myImage3`, SpriteKind.Food);
+food.x = randint(8, 140)
+food.y = randint(8, 120)
+// #1
 player = sprites.create(assets.image`myImage`, SpriteKind.Player);
-player.x = 120
-player.y = 80
+player.x = playerXLocation
+player.y = playerYLocation
 controller.moveSprite(player);
 player.setFlag(SpriteFlag.StayInScreen, true);
+// ======================= EVENT HANDLERS ======================= \\
+// #4
+// #6
+game.onUpdateInterval(15000, function () {
+    CreateEnemies()
+    CreateOtherEnemies()
+ })
 
-for (let i = 8; i < 160; i += 16){
-    enemy = new sprites.ExtendableSprite(assets.image`myImage1`, SpriteKind.Enemy) //sprites.create(assets.image`myImage1`, SpriteKind.Enemy);
-    enemy.x = i;
-    enemy.y = 8
-}
-
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function(sprite: Sprite, otherSprite: Sprite) {
-    sprite.destroy()
-    otherSprite.destroy()
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (player: Sprite, food: Sprite) {
+    food.destroy()
+    FoodEaten = true
+})
+// #3
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Projectile, function (enemy: EnemySprite, projectile: Sprite) {
+    projectile.destroy()
+    // #10
+    if (FoodEaten) {
+        enemy.hitpoints -= 2
+    } else {
+        enemy.hitpoints--
+        if (enemy.hitpoints == 1) {
+            // #11
+            enemy.setImage(assets.image`myImage2`)
+        }
+    }
+    if (enemy.hitpoints <= 0) {
+        enemy.destroy(effects.confetti, 250)
+    }
 })
 
+sprites.onOverlap(SpriteKind.otherEnemy, SpriteKind.Projectile, function (enemy: EnemySprite, projectile: Sprite) {
+    projectile.destroy()
+    // #10
+    if (FoodEaten) {
+        enemy.hitpoints -= 2
+    } else {
+        enemy.hitpoints--
+    }
+    if (enemy.hitpoints <= 0) {
+        enemy.destroy(effects.confetti, 250)
+    }
+})
+// #2
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.create(assets.image`myImage0`, SpriteKind.Projectile);
     projectile.setFlag(SpriteFlag.AutoDestroy, true);
     projectile.x = player.x;
     projectile.y = player.y;
-    projectile.vy = -100;
+    projectile.vy = projectileVelocity;
 })
+// ======================= FUNCTIONS ======================= \\
+// #5
+function CreateEnemies() {
+    ghostenemy =
+        new EnemySprite(assets.image`myImage1`, SpriteKind.Enemy) //sprites.create(assets.image`myImage1`, SpriteKind.Enemy);
+    // #9
+    ghostenemy.hitpoints = ghostenemyHitpoints
+    ghostenemy.x = ghostenemyXLocation;
+    ghostenemy.y = ghostenemyYLocation;
+    return ghostenemy
+}
+// #13
+function CreateOtherEnemies() {
+    otherenemy =
+        new EnemySprite(assets.image`myImage4`, SpriteKind.otherEnemy)
+    otherenemy.hitpoints = otherenemyHitpoints
+    otherenemy.x = otherenemyXLocation
+    otherenemy.y = otherenemyYLocation
+    return otherenemy
+}
 
